daa96e96304d39abeeaf4ebeed9dadb48678f7f7
Vivaan Baid <baidvivaan782@gmail.com>
2022-02-21 01:02:20 -0500

    Initial Commit

---
+++ WorldScramble/WorldScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldScrambleApp.swift
+//  WorldScramble
+//
+//  Created by Vivaan Baid on 21/02/22.
+//
+
+import SwiftUI
+
+@main
+struct WorldScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
